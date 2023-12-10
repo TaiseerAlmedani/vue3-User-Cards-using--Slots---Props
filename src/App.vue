eb444b14012d84c8eb568b0c1ec3c9dc02e182b1
TAISEER ALMEDANI <taiseer.almedani@gmail.com>
2023-12-10 22:20:10 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+import UserCard from './components/UserCard.vue';
+export default{
+components:{
+  UserCard
+},
+data(){
+  return{
+    usersFromApi:[]
+  }
+},
+created(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.usersFromApi = users)
+}
+}
+</script>
+<template>
+  <section>
+    <div class="row">
+      <h1>Our Team</h1>
+    </div>
+    <div class="row">
+      <UserCard 
+      :twitter="user.address.street"
+      :linkedin="user.name"
+      :github="user.address.zipcode"
+      :envelope="user.address.city"
+       v-for="user in usersFromApi"
+       :key="user.id">
+        <template #fullName>
+          {{ user.name }}
+        </template>
+        <template #role>
+          {{ user.username }}
+        </template>
+        <template #brief>
+          {{user.email}}
+        </template>
+      </UserCard>
+    </div>
+  </section>
+</template>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,51 @@
+<script>
+export default {
+  props:{
+    twitter:{
+      type:String,
+      default:'https://www.twitter.com',
+    },
+    linkedin:{
+      type:String,
+      default:'https://www.linkedin.com'
+    },
+    envelope:{
+      type:String,
+      default:'https://www.envelope.com'
+    },
+    github:{
+      type:String,
+      default:'https://www.github.com'
+    },
+    name:String
+  }
+}
+</script>
+<template>
+  <div class="column">
+    <div class="card">
+      <div class="img-container">
+        <img src="@/assets/avatar.png" />
+      </div>
+      <h3><slot name="fullName">John doe</slot></h3>
+      <p><slot name ="role">nothinng</slot></p>
+      <p class="brief">
+        <slot name="brief">Lorem, ipsum dolor.</slot>
+      </p>
+      <div class="icons">
+        <a :href="twitter">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a :href="linkedin">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a :href="github">
+          <i class="fab fa-github"></i>
+        </a>
+        <a :href="envelope">
+          <i class="fas fa-envelope"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
